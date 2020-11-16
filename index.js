1f2a0f6bb0172640852abc2bd6bc77c99410c361
WeicaiZhao <wz1842@mun.ca>
2020-11-16 10:51:03 -0330

    add cors &koaBody

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const cors = require('@koa/cors')
+const koaBody = require('koa-body')
+
 const app = new Koa()
 const router = new Router()
 
@@ -20,6 +23,19 @@ router.get('/async', async (ctx) =>{
     }))
     ctx.body = result
 })
+
+router.post('/post', async (ctx) => {
+    let {body} = ctx.request
+    console.log(body);
+    console.log(ctx.request);
+    ctx.body = {
+        ... body
+    }
+})
+
+app.use(koaBody())
+
+app.use(cors())
 
 app.use(router.routes())
     .use(router.allowedMethods())
